782f7ec0bf38e652a2855c07039d34cf5d1b79f1
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-30 19:41:49 -0300

    Birthday Parte 6: Styles.js e uninstall react-router-dom

---
+++ birthday/src/Styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 18,
+    marginVertical: 2,
+  },
+
+  subtitle: {
+    fontSize: 14,
+  },
+});
+
+export default Styles;
