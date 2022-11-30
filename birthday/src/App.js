957721ae21dbeee0bbf416b0dbe3130945e53c29
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-30 16:06:26 -0300

    Birthday Parte 2: react-router-dom

---
+++ birthday/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>Birthday</h1>
+          <h2>Time</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
